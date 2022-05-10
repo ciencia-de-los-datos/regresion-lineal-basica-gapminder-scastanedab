e5e0dcc6ff1676d3206a190fed58544fcaded56b
scastanedab <101910033+scastanedab@users.noreply.github.com>
2022-05-09 22:14:22 -0500

    Update preguntas.py
    1-2

--- preguntas.py
+++ preguntas.py
@@ -73,7 +73,7 @@ def pregunta_03():
     """
 
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = ____
+    df = pd.read_csv("gm_2008_region.csv")
 
     # Asigne a la variable los valores de la columna `fertility`
     X_fertility = ____
